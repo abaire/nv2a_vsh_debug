4b3e42fac5982ce7b99539315283e55e1cec3e05
Erik Abair <abaire@users.noreply.github.com>
2023-06-10 23:17:58 -0700

    Use tables for output panel. (#17)

--- src/nv2adbg/_output_panel.py
+++ src/nv2adbg/_output_panel.py
@@ -7,6 +7,7 @@ from rich.console import RenderableType
 from rich.layout import Layout
 from rich.rule import Rule
 from rich.style import Style
+from rich.table import Table
 from rich.text import Text
 from textual.geometry import Size
 from textual.widgets import Static
@@ -23,7 +24,13 @@ class _OutputPanel(Static):
         self.update()
 
     def get_content_height(self, container: Size, viewport: Size, width: int) -> int:
-        return 2
+        if not self._step:
+            return 2
+
+        ret = 0
+        for outputs in self._step.outputs.values():
+            ret = max(ret, len(outputs))
+        return 1 + ret
 
     def render(self) -> RenderableType:
         if not self._step:
@@ -31,11 +38,11 @@ class _OutputPanel(Static):
         else:
             outputs = self._step.outputs
             composables = [
-                self._render_output(stage, outputs[stage])
+                self._render_register_values(stage, outputs[stage])
                 for stage in sorted(outputs.keys(), reverse=True)
             ]
 
-        layout = Layout(size=2, ratio=0)
+        layout = Layout()
         layout.split_column(
             Rule("Outputs"),
             Layout(
@@ -43,15 +50,23 @@ class _OutputPanel(Static):
                     composables,
                     expand=True,
                 ),
-                size=1,
             ),
         )
         return layout
 
-    def _render_output(
+    def _render_register_values(
         self, stage_name: str, registers: List[RegisterReference]
     ) -> RenderableType:
-        elements = [(f"{stage_name}: ", "bold")]
+        outer = Table.grid()
+        outer.add_column()
+        outer.add_column()
+
+        inner = Table.grid()
+        inner.add_column()
+        inner.add_column()
+        inner.add_column()
+        inner.add_column()
+        inner.add_column()
 
         theme = self.app.get_css_variables()
         muted_suffix = "darken-2" if self.app.dark else "lighten-1"
@@ -63,10 +78,11 @@ class _OutputPanel(Static):
         state = self._step.state
 
         for register in registers:
-            elements.append((register.raw_name, register_style))
+            elements = [(register.raw_name, register_style)]
             if register.sorted_mask != "xyzw":
                 elements.append((".", muted_register_style))
                 elements.append((register.sorted_mask, register_style))
+            row = [Text.assemble(*elements)]
 
             def get_value_style(component: str) -> Style:
                 return (
@@ -76,12 +92,12 @@ class _OutputPanel(Static):
                 )
 
             x, y, z, w = state.get(f"{register.canonical_name}")
-            elements.append((f" {x}", get_value_style("x")))
-            elements.append((f", ", muted_value_style))
-            elements.append((f" {y}", get_value_style("y")))
-            elements.append((f", ", muted_value_style))
-            elements.append((f" {z}", get_value_style("z")))
-            elements.append((f", ", muted_value_style))
-            elements.append((f" {w}", get_value_style("w")))
+            row.append(Text(f" {x}", style=get_value_style("x")))
+            row.append(Text(f" {y}", style=get_value_style("y")))
+            row.append(Text(f" {z}", style=get_value_style("z")))
+            row.append(Text(f" {w}", style=get_value_style("w")))
 
-        return Text.assemble(*elements)
+            inner.add_row(*row)
+
+        outer.add_row(Text(f"{stage_name}: ", style="bold"), inner)
+        return outer
